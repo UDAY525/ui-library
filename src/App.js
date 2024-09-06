421e10dd66f074ea7512458a5554238cc5026a5a
uday <udaybikkinauday54359@gmail.com>
2024-09-06 23:03:52 +0530

    star ratings and image slider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import Accordion from './components/accordian';
-import RandomColor from './components/randomColorGenerator';
+import RandomColor from './components/randomColor';
+import StarRatings from './components/starRatings';
+import ImageSlider from './components/image-slider';
 function App() {
   return (
     <div className="App">
@@ -9,7 +10,17 @@ function App() {
       {/* <Accordion/> */}
 
       {/* Random color generator */}
-      <RandomColor/>
+      {/* <RandomColor/> */}
+
+      {/* Star ratings */}
+      {/* <StarRatings noOfStars={10}/> */}
+
+      {/* Image Slider */}
+      <ImageSlider
+      url={"https://picsum.photos/v2/list"}
+      page={"1"}
+      limit={"10"}
+      />
     </div>
   );
 }
